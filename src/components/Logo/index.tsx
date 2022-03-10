07805e578c57e729ca63883cad851278a5f917ce
Jessy Avalos Barrales <jessyavalos16@outlook.es>
2022-03-09 19:26:02 -0600

    Login started

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+import PetLogo from './pet-logo/pet-logo.png';
+
+const Logo = () => {
+    return (
+        <img src={PetLogo} alt="PetLogo"/>
+    );
+}
+
+export default Logo;
--- src/components/PetCardContainer/index.tsx
+++ src/components/PetCardContainer/index.tsx
@@ -1,7 +1,7 @@
 import './index.css';
 
 import Pagination from 'react-responsive-pagination';
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import PetCard from './PetCard/index';
@@ -23,15 +23,13 @@ const PetCardContainer = () => {
     const [breeds, setBreeds] = useState([]);
 
     useEffect(() => {
-        axios.get(`https://api.thedogapi.com/v1/breeds?limit=10&page=${currentPage - 1}`)
+        axios.get(`${process.env.REACT_APP_DOG_URL}/v1/breeds?limit=10&page=${currentPage - 1}`)
             .then((data) => {
                 setBreeds(data.data);
                 setTotalCount(Math.floor(Number(data.headers['pagination-count']) / 10));
             })
             .catch((error) => console.log(error));
     }, [currentPage]);
-
-    const handlePageChange = (page: number) => setCurrentPage(page);
 
     return (
         <>
